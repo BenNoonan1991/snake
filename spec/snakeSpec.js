66a8d3ce39b958e2cebf163147ef21c8ebf03dfb
danielemanuel <daniel.costea@vkernel.ro>
2017-06-05 15:27:58 +0100

    Default score test

--- spec/snakeSpec.js
+++ spec/snakeSpec.js
@@ -8,4 +8,8 @@ describe('Snake', function() {
   it('has a default size of 10', function(){
     expect(snake.defaultSnakeSize()).toEqual(10);
   });
+
+  it('has a default score of 0', function() {
+    expect(snake.defaultScore()).toEqual(0);
+  });
 });
