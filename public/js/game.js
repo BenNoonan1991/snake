41ff9d68ca93fd913e17795abb441a7b1da262e2
Adam Erdem Er <adamerdemer@gmail.com>
2017-06-06 16:24:30 +0100

    Added requestAnimationFrame

--- public/js/game.js
+++ public/js/game.js
@@ -23,9 +23,22 @@
 		var screen = canvas.getContext('2d');
 		var gameSize = { x: canvas.width, y: canvas.height };
 
-		console.log('hello');
+		var self = this;
+		var tick = function() {
+			self.update();
+			self.draw(screen, gameSize);
+			requestAnimationFrame(tick);
+		};
+		tick();
 	};
 	Game.prototype= {
+		update: function(){
+			console.log("hi")
+		},
+
+		draw: function(){
+			
+		}
 
 	};
 
