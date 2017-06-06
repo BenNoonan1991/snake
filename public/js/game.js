fd846b1b31c8aa4fc2b5d3bdd182493f993ed98c
Adam Erdem Er <adamerdemer@gmail.com>
2017-06-06 17:24:05 +0100

    Added ability to go left or right

--- public/js/game.js
+++ public/js/game.js
@@ -34,12 +34,16 @@
 		tick();
 	};
 	Game.prototype= {
-		update: function(){
-			console.log("hi")
+		update: function() {
+			for (var i = 0; i < this.bodies.length; i++) {
+				this.bodies[i].update();
+			}
 		},
 
 		draw: function(screen, gameSize){
-			screen.fillRect(30, 30, 40, 40);
+			for (var i = 0; i < this.bodies.length; i++) {
+				drawRect(screen, this.bodies[i]);
+			}
 		}
 	};
 
@@ -47,14 +51,43 @@
 		this.game = game;
 		this.size = { x:15, y:15 };
 		this.center = { x:gameSize.x / 2, y:gameSize.y / 2 };
+		this.keyboarder = new Keyboarder();
 	};
 
 	Snake.prototype = {
 		update: function() {
+			if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+				this.center.x -= 2;
+			} else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+				this.center.x += 2;
+			}
 
 		},
 	};
 
+	var drawRect = function(screen, body) {
+		screen.fillRect(body.center.x - body.size.x / 2,
+										body.center.y - body.size.y / 2,
+										body.size.x, body.size.y);
+	};
+
+	var Keyboarder = function() {
+		var keyState = {};
+
+		window.onkeydown = function(e) {
+			keyState[e.keyCode] = true;
+		};
+
+		window.onkeyup = function(e) {
+			keyState[e.keyCode] = false;
+		};
+
+		this.isDown = function(keyCode) {
+			return keyState[keyCode] === true;
+		};
+
+		this.KEYS = { LEFT: 37, RIGHT: 39};
+	};
 
 	window.onload = function() {
 		new Game("screen");
