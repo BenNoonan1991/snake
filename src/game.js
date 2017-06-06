b4e4df0e3d411dfe3b030a9e195ec3f758638f9b
danielemanuel <daniel.costea@vkernel.ro>
2017-06-06 13:55:50 +0100

    Testing the snake display

--- src/game.js
+++ src/game.js
@@ -1,19 +1,24 @@
+
+
 var canvas = document.getElementById("myCanvas"),
 	ctx = canvas.getContext("2d"),
 		//Full width and height
 	w = window.innerWidth,
 	h = window.innerHeight;
-	// canvas.height = h;
-	// canvas.width = w;
+// 	// canvas.height = h;
+// 	var drawSnake = function(snakeToDraw) {
+//    var drawableSnake = { color: "green", pixels: snakeToDraw };
+//    var drawableObjects = [drawableSnake];
+//   CHUNK.draw(drawableObjects);
+// };
 
+// var snake = [{ top: 0, left: 0}];
+// CHUNK.executeNTimesPerSecond(advanceGame, 1);
 
-ctx.font = "30px Arial";
-ctx.strokeText("Hello World",10,50);
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"red");
+grd.addColorStop(1,"white");
 
-	var drawSnake = function(snakeToDraw) {
-    var drawableSnake = { color: "green", pixels: snakeToDraw };
-    var drawableObjects = [drawableSnake];
-    CHUNK.draw(drawableObjects);
-  }
-  var snake = [{ top: 0, left: 0}];
-  drawSnake(snake)
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,50,50);
