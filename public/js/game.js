d787352df12168856ce0e3e253ecb52311c8ec26
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-06-06 16:45:11 +0100

    adds snake prototype

--- public/js/game.js
+++ public/js/game.js
@@ -23,6 +23,8 @@
 		var screen = canvas.getContext('2d');
 		var gameSize = { x: canvas.width, y: canvas.height };
 
+		this.bodies = [new Snake(this, gameSize)];
+
 		var self = this;
 		var tick = function() {
 			self.update();
@@ -36,11 +38,24 @@
 			console.log("hi")
 		},
 
-		draw: function(){
-			
+		draw: function(screen, gameSize){
+			screen.fillRect(30, 30, 40, 40);
 		}
-
 	};
+
+	var Snake = function(game, gameSize) {
+		this.game = game;
+		this.size = { x:15, y:15 };
+		this.center = { x:gameSize.x / 2, y:gameSize.y / 2 };
+	};
+
+	Snake.prototype = {
+		update: function() {
+
+		}
+	};
+
+};
 
 	window.onload = function() {
 		new Game("screen");
