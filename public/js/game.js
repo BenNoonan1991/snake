868d31574a6d4eda303ebb20258c3540f125e515
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-06-06 16:15:04 +0100

    starts constructor function

---
+++ public/js/game.js
@@ -0,0 +1,35 @@
+// var canvas = document.getElementById("myCanvas"),
+// 	ctx = canvas.getContext("2d"),
+// 		//Full width and height
+// 	w = window.innerWidth,
+// 	h = window.innerHeight;
+// 	// canvas.height = h;
+// 	// canvas.width = w;
+//
+//
+// ctx.font = "30px Arial";
+// ctx.strokeText("Hello World",10,50);
+//
+// 	var drawSnake = function(snakeToDraw) {
+//     var drawableSnake = { color: "green", pixels: snakeToDraw };
+//     var drawableObjects = [drawableSnake];
+//     CHUNK.draw(drawableObjects);
+//   }
+//   var snake = [{ top: 0, left: 0}];
+//   drawSnake(snake)
+;(function() {
+	var Game = function(canvasId) {
+		var canvas = document.getElementById(canvasId);
+		var screen = canvas.getContext('2d');
+		var gameSize = { x: canvas.width, y: canvas.height };
+
+		console.log('hello');
+	};
+	Game.prototype= {
+
+	};
+
+	window.onload = function() {
+		new Game("screen");
+	};
+})();
