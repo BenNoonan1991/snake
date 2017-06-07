8a259e87124aeb372b2f19f9f07ba5f71ac51a3e
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-06-07 10:31:47 +0100

    adds food constructor function

--- public/js/game.js
+++ public/js/game.js
@@ -1,10 +1,12 @@
 ;(function() {
+  var BLOCK_SIZE = 10;
+
 	var Game = function(canvasId) {
 		var canvas = document.getElementById(canvasId);
 		var screen = canvas.getContext('2d');
 		var gameSize = { x: canvas.width, y: canvas.height };
 
-		this.bodies = [new Snake(this, gameSize)];
+		this.bodies = [new Snake(this, gameSize), this.addFood()];
 
 		var self = this;
 		var tick = function() {
@@ -21,17 +23,25 @@
 			}
 		},
 
-		draw: function(screen, gameSize){
+		draw: function(screen, gameSize) {
 			screen.clearRect(0, 0, gameSize.x, gameSize.y);
 			for (var i = 0; i < this.bodies.length; i++) {
 				drawRect(screen, this.bodies[i]);
 			}
+		},
+
+		addBody: function(body) {
+			this.bodies.push(body);
+		},
+
+		addFood: function() {
+			this.addBody(new Food(this));
 		}
 	};
 
 	var Snake = function(game, gameSize) {
 		this.game = game;
-		this.size = { x:15, y:15 };
+		this.size = { x: BLOCK_SIZE, y: BLOCK_SIZE };
 		this.center = { x:gameSize.x / 2, y:gameSize.y / 2 };
 		this.keyboarder = new Keyboarder();
 	};
@@ -49,6 +59,25 @@
 			}
 
 		},
+	};
+
+	var Food = function(game, GameSize) {
+		this.game = game;
+
+		while(this.center === undefined) {
+			var center = this.game.randomSquare();
+			if (this.game.isSquareFree(center)) {
+				this.center = center;
+			}
+		}
+
+		this.size = { x: BLOCK_SIZE, y: BLOCK_SIZE };
+	};
+
+	Food.prototype = {
+		draw: function(screen) {
+			drawRect(screen, this, "green")
+		}
 	};
 
 	var drawRect = function(screen, body) {
