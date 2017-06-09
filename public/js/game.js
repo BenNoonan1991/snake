ed339873db2bc0fe1d0a0d3b5e685ba1e047eb30
danielemanuel <daniel.costea@vkernel.ro>
2017-06-09 10:02:42 +0100

    Unecesary files deleted

--- public/js/game.js
+++ public/js/game.js
@@ -275,7 +275,9 @@
     screen.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,
                     body.size.x, body.size.y);
   };
-
+	// button.onclick = function {
+	// 	new Game('screen')
+	// }
 	window.onload = function() {
 		new Game("screen");
 	};
--- spec/snakeSpec.js
+++ spec/snakeSpec.js
@@ -1,19 +1,22 @@
-describe('Snake', function() {
-  var snake;
+'use strict'
 
+describe('Snake', function() {
+  // var game;
+  var snake;
   beforeEach(function() {
+    // game = new Game();
     snake = new Snake();
   });
 
   it('has a default size of 10', function(){
-    expect(snake.defaultSnakeSize()).toEqual(10);
+    expect(snake.blocks).toEqual([]);
   });
-
-  it('has a default score of 0', function() {
-    expect(snake.defaultScore()).toEqual(0);
-  });
-
-  it('defaults to an empty array', function() {
-    expect(snake.size).toEqual([]);
-  })
+  //
+  // it('has a default score of 0', function() {
+  //   expect(snake.defaultScore()).toEqual(0);
+  // });
+  //
+  // it('defaults to an empty array', function() {
+  //   expect(snake.size).toEqual([]);
+  // })
 });
